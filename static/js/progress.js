4cb78035b56e4294e7148e378c2d7b01ca72310b
tiansong_chen <tiansong_chen@kingdee.com>
2015-04-01 16:03:34 +0800

    progress 增加显示进度条件

--- static/js/progress.js
+++ static/js/progress.js
@@ -8,6 +8,8 @@ var progress = function(a) {
 	var doc = window.document, obj = doc.getElementById("progress");
 	obj || (obj = doc.createElement("div"), obj.id = "progress", obj.appendChild(doc.createElement("dt")), obj.appendChild(doc.createElement("dd")),doc.body.appendChild(obj));
 	var time = a[0], percent = a[1], cls = a[2];
+	if( percent < Number(obj.dataset.width )) return true;
+	obj.dataset.width = percent;
 	obj.className = "";
 	var objStyle = obj.style;
 	objStyle.transitionDuration = objStyle.webkitTransitionDuration = time + "ms";
